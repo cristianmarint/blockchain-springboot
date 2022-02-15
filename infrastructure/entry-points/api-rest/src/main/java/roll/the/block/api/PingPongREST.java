9328b008810fa703e3442f237732a1e72a972774
Cristian Marin <cristianmarint@gmail.com>
2022-02-15 17:40:44 -0500

    ðPing Pong c:

--- infrastructure/entry-points/api-rest/src/main/java/roll/the/block/api/PingPongREST.java
+++ infrastructure/entry-points/api-rest/src/main/java/roll/the/block/api/PingPongREST.java
@@ -5,6 +5,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roll.the.block.model.rest.models.GenericData;
+import roll.the.block.model.rest.models.GenericResponse;
 
 import static roll.the.block.model.constants.Endpoints.URL_BASE_V1;
 
@@ -14,7 +16,14 @@ import static roll.the.block.model.constants.Endpoints.URL_BASE_V1;
 public class PingPongREST {
 
     @GetMapping(path = "/ping")
-    public String commandName() {
-        return "Pong \uD83C\uDFD3";
+    public GenericResponse commandName() {
+
+        GenericData response = GenericData.builder()
+                .message("Pong \uD83C\uDFD3")
+                .build();
+
+        return GenericResponse.builder()
+                .data(response)
+                .build();
     }
 }
